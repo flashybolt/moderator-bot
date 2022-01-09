affae30df39c07bb078adc3dabdfd58f6a97db79
sc-github-dev <wodaguge369@gmail.com>
2022-01-09 16:13:05 -0500

    Removed super secret firebase data, started service account.

--- src/helper.js
+++ src/helper.js
@@ -1,32 +1,21 @@
-/*const firebase = require('firebase');
-const { initializeApp } = require("firebase/app");
-const { getDatabase } = require("firebase/database");
+const admin = require('firebase-admin');
+const serviceAccount = require('../moderator-bot-336622-53623ae6a339.json');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCd1E9T92ey5DCet8CQM7iLjoY3WsUzcJ8",
-  authDomain: "moderator-bot-5d0c3.firebaseapp.com",
-  databaseURL: "https://moderator-bot-5d0c3-default-rtdb.firebaseio.com",
-  projectId: "moderator-bot-5d0c3",
-  storageBucket: "moderator-bot-5d0c3.appspot.com",
-  messagingSenderId: "864458254138",
-  appId: "1:864458254138:web:d38dd91e776e6856345203",
-  measurementId: "${config.measurementId}"
-};
+console.log(process.env.FIREBASE_DATABASEURL);
+console.log();
 
-const app = initializeApp(firebaseConfig);
-const db = firebase.database();
-const obj = {'test':'poop'}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASEURL
+});
 
-db.ref("customPath").set(obj, function(error) {
-    if (error) {
-      // The write failed...
-      console.log("Failed with error: " + error)
-    } else {
-      // The write was successful...
-      console.log("success")
-    }
-})
-*/
+const db = admin.database();
+////////////////////////////////////////////////
+const warnsref = db.ref('warns/');
+warnsref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
 ////////////////////////////////////////////////
 module.exports = {
   command: function(msg, keyword, func){
